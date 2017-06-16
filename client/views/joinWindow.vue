b795ee3b852db1672a3e0463ea33975d79b2b070
VictoriaChou <18818218114@163.com>
2017-06-16 09:22:55 +0800

    加入聊天室窗口

---
+++ client/views/joinWindow.vue
@@ -0,0 +1,32 @@
+<template>
+<main>
+    <input type="text" v-model="name">
+    <button @click="joinRoom">Join</button>
+</main>
+</template>
+
+<script>
+export default {
+    name: 'joinWindow',
+    props: [ 'socketBus' ],
+    data () {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        joinRoom: function() {
+            this.socketBus.emit('chatter joined!', { name: this.name })
+        }
+    }
+}
+</script>
+
+<style scoped>
+main {
+    /*background-color: grey;*/
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
