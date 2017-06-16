a632f82773abfe8dc37937792b27d8fcca5cecd3
VictoriaChou <18818218114@163.com>
2017-06-16 09:06:51 +0800

    聊天室

---
+++ client/views/chatterRoom.vue
@@ -0,0 +1,24 @@
+<template>
+	<main>
+		<chatter-window
+			v-for="(chatter, index) in chatters"
+		></chatter-window>
+		<center-window></center-window>
+	</main>
+</template>
+
+<script>
+	import CONFIG from '../../config.js'
+
+	import ChatterWindow from '../components/ChatterWindow.vue'
+	import CenterWindow from '../components/CenterWindow.vue'
+
+	export default {
+		name: 'chatterroom',
+		components: {
+			ChatterWindow,
+			CenterWindow
+		},
+		props: [ 'socketBus', 'chatters']
+	}
+</script>
