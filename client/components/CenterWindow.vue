23ae84b38aba67cb797dbcd3261ad5fc7f632564
VictoriaChou <18818218114@163.com>
2017-06-16 09:23:59 +0800

    中间窗口

---
+++ client/components/CenterWindow.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="center">
+</div>
+</template>
+
+<script>
+export default {
+    name: 'CenterWindow',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+.center {
+    display: inline-block;
+    border-radius: 100%;
+    border: 1px solid grey;
+    position: absolute;
+    transition: 1s;
+}
+</style>
